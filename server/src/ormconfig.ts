4c70c2489b2bd933f7d1907ad3cb90a7802f725e
DamnStraight <ste77236@gmail.com>
2022-02-21 21:19:54 -0500

    Configured eslint, added test db to docker, updated ormconfig

---
+++ server/src/ormconfig.ts
@@ -0,0 +1,32 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+// Add type safety to our process.env calls
+export const env = (name: keyof ProjectEnv, fallback?: string): string => {
+  const val = process.env[name];
+
+  if (!val) {
+    
+    if (fallback) {
+      return fallback;
+    }
+      
+    throw new Error(`Missing environment variable [${name}]`);
+  }
+
+  return val;
+};
+
+export const ormconfig: PostgresConnectionOptions = {
+  type        : 'postgres',
+  port        : Number(env('DB_PORT')),
+  host        : env('DB_HOST', 'localhost'),
+  database    : env('DB_NAME'),
+  synchronize : env('DB_SYNCHRONIZE') === 'true',
+  logging     : env('DB_LOGGING') === 'true',
+  dropSchema  : env('DB_DROPSCHEMA') === 'true',
+  username    : env('DB_USERNAME'),
+  password    : env('DB_PASSWORD'),
+  entities    : ['src/entity/**/*.ts'],
+  migrations  : ['src/migration/**/*.ts'],
+  subscribers : ['src/subscriber/**/*.ts'],
+};
---
+++ server/src/types/environment.d.ts
@@ -0,0 +1,18 @@
+type Environment = 'dev' | 'prod' | 'test';
+
+interface ProjectEnv {
+  NODE_ENV: Environment;
+  LOG_LEVEL: 'info' | 'error' | 'silent';
+  DB_NAME: string;
+  DB_SYNCHRONIZE: string;
+  DB_DROPSCHEMA: string;
+  DB_LOGGING: string;
+  DB_PORT: string;
+  DB_HOST: string
+  DB_USERNAME: string
+  DB_PASSWORD: string
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv extends Partial<ProjectEnv> {}
+}
